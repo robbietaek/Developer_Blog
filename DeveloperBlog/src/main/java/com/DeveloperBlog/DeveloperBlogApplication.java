dba114b681f71c681fbbdfc7acd5a2ec9637496b
Robbie <Robbie@192.168.25.27>
2021-05-20 21:31:08 +0900

    최초 프로젝트 생성 및 SpringBoot환경설정

---
+++ DeveloperBlog/src/main/java/com/DeveloperBlog/DeveloperBlogApplication.java
@@ -0,0 +1,13 @@
+package com.DeveloperBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperBlogApplication.class, args);
+	}
+
+}
